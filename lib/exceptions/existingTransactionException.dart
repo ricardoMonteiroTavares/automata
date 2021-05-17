87163d17324a91e4b5c66173f5a70f3e5a1bb16e
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-05-16 22:38:50 -0300

    Ajuste na doc do ExistingTransactionException

--- lib/exceptions/existingTransactionException.dart
+++ lib/exceptions/existingTransactionException.dart
@@ -1,4 +1,5 @@
-/// Classe de exceção caso já exista uma transação em um autômato
+/// Classe de exceção caso já exista uma transação em um autômato, seja ela idêntica ou
+/// se tem o parâmetro da transação presente no estado de partida.
 class ExistingTransactionException implements Exception {
   final String _msg = "Transação Existente";
 
