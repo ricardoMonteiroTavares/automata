ff553989fe35365d3cf06d29fe8baa816b5105df
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-05-17 20:55:02 -0300

    Testes para o objeto de Testes Unitários

---
+++ test/models/unitTest_test.dart
@@ -0,0 +1,25 @@
+import 'package:automata/enums/target.dart';
+import 'package:automata/models/unitTest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  UnitTest obj = UnitTest();
+  test('Criar o objeto UnitTest', () {
+    expect(obj, isA<UnitTest>());
+  });
+
+  test('Modificar o valor do input', () {
+    obj.input = "abcdef";
+    expect(obj.input, "abcdef");
+  });
+
+  test('Modificar o valor do debug', () {
+    obj.debug = true;
+    expect(obj.debug, true);
+  });
+
+  test('Modificar o valor do target', () {
+    obj.target = Target.failure;
+    expect(obj.target, Target.failure);
+  });
+}
