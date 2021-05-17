d640fec8576b54b428f87cfee1f17c7f15e70d17
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-05-17 11:44:25 -0300

    Ajuste na doc da classe de transação

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,7 +1,5 @@
-/**
- * Objeto responsável por uma transação entre estados de um autômato.
- * Ela descreve uma ligação entre um estado de partida (from) e de chegada (to) através de um valor de gatilho (parameter).
- */
+/// Objeto responsável por uma transação entre estados de um autômato.
+/// Ela descreve uma ligação entre um estado de partida (from) e de chegada (to) através de um valor de gatilho (parameter).
 class Transaction {
   String _from = "";
   String _to = "";
