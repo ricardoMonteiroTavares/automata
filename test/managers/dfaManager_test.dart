005346452361c42488c759800a2fccdadf70ed03
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-05-17 11:45:24 -0300

    Criação do arquivo de testes do manager de AFD

---
+++ test/managers/dfaManager_test.dart
@@ -0,0 +1,112 @@
+import 'package:automata/managers/interfaces/dfaManager.dart';
+import 'package:automata/models/transaction.dart';
+import 'package:test/test.dart';
+
+void main() {
+  DFAManager obj = DFAManager();
+  test('Criar o objeto AFD', () {
+    obj.newDFA();
+
+    expect(obj.dfa!.initialState, "");
+    expect(obj.dfa!.states, []);
+    expect(obj.dfa!.transactions, []);
+    expect(obj.dfa!.finalStates, []);
+  });
+
+  test('Adicionar estados no AFD', () {
+    obj.addState("q0");
+    obj.addState("q1");
+
+    expect(obj.dfa!.states.length, 2);
+    expect(obj.dfa!.states, ["q0", "q1"]);
+  });
+
+  test('Modificar o estado inicial do AFD', () {
+    obj.setInitialState("q0");
+
+    expect(obj.dfa!.initialState, "q0");
+  });
+
+  test('Adicionar um estado final no AFD', () {
+    obj.addFinalState("q1");
+
+    expect(obj.dfa!.finalStates.length, 1);
+    expect(obj.dfa!.finalStates, ["q1"]);
+  });
+
+  test('Adicionar uma transação', () {
+    Transaction t = Transaction(from: "q0", to: "q1", parameter: "0");
+
+    obj.addTransaction(t);
+
+    expect(obj.dfa!.transactions.length, 1);
+    expect(obj.dfa!.transactions, ["0"]);
+    expect(obj.dfa!.tableTransactions, {
+      "q0": {"0": "q1"}
+    });
+  });
+
+  test('Modificar uma transação já existente', () {
+    obj.addState("q2");
+    Transaction before = Transaction(from: "q0", to: "q1", parameter: "0");
+    Transaction after = Transaction(from: "q0", to: "q2", parameter: "0");
+
+    obj.setTransaction(before, after);
+
+    expect(obj.dfa!.states.length, 3);
+    expect(obj.dfa!.states, ["q0", "q1", "q2"]);
+
+    expect(obj.dfa!.transactions.length, 1);
+    expect(obj.dfa!.transactions, ["0"]);
+    expect(obj.dfa!.tableTransactions, {
+      "q0": {"0": "q2"}
+    });
+  });
+
+  test('Remover uma transação existente na tabela de transações', () {
+    Transaction t = Transaction(from: "q0", to: "q2", parameter: "0");
+    obj.removeTransaction(t);
+
+    expect(obj.dfa!.transactions.length, 0);
+    expect(obj.dfa!.tableTransactions, {"q0": {}});
+  });
+
+  test('Remover um estado final', () {
+    obj.removeFinalState("q1");
+
+    expect(obj.dfa!.finalStates.length, 0);
+  });
+
+  test(
+      'Remover um estado sem transações na tabela de transações e que não é estado final',
+      () {
+    obj.removeState("q2");
+
+    expect(obj.dfa!.states.length, 2);
+    expect(obj.dfa!.states, ["q0", "q1"]);
+  });
+
+  test(
+      'Remover um estado sem transações na tabela de transações e que é estado final',
+      () {
+    obj.addFinalState("q1");
+    obj.removeState("q1");
+
+    expect(obj.dfa!.states.length, 1);
+    expect(obj.dfa!.states, ["q0"]);
+
+    expect(obj.dfa!.finalStates.length, 0);
+  });
+
+  test('Remover um estado que tem transações na tabela de transações e que é estado inicial', () {
+    obj.addState("q1");
+    Transaction t = Transaction(from: "q0", to: "q1", parameter: "0");
+    obj.addTransaction(t);
+    obj.removeState("q0");
+
+    expect(obj.dfa!.states.length, 1);
+    expect(obj.dfa!.states, ["q1"]);
+    expect(obj.dfa!.transactions.length, 0);
+    expect(obj.dfa!.tableTransactions, {});
+  });
+}
