dd034c35b009ca0754294e66bd6e1dbda85ddecf
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-05-20 12:04:21 -0300

    Conversão de stateless para stateful widget o objeto de estado para poder possibilitar a troca de cor ao selecionar o objeto

--- lib/widgets/stateWidget.dart
+++ lib/widgets/stateWidget.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
 
-class StateWidget extends StatelessWidget {
+class StateWidget extends StatefulWidget {
+  late final Function _f;
+  late final int _id;
   late final String _name;
-  final double _size = 60;
-  StateWidget({required String name}) {
+
+  StateWidget({required int id, required String name, required Function f}) {
+    _id = id;
     _name = name;
+    _f = f;
+  }
+
+  int get id => _id;
+
+  @override
+  StateWidgetState createState() => StateWidgetState();
+}
+
+class StateWidgetState extends State<StateWidget> {
+  final double _size = 60;
+
+  Color _color = Colors.black;
+
+  updateColor() {
+    setState(() {
+      if (_color == Colors.black) {
+        _color = Colors.blue;
+      } else {
+        _color = Colors.black;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: _size,
-      width: _size,
-      child: Center(
-        child: Text(_name),
+    return GestureDetector(
+      onTap: () => {widget._f(this)},
+      child: Container(
+        height: _size,
+        width: _size,
+        child: Center(
+          child: Text(widget._name),
+        ),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: _color, width: 2.0)),
       ),
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.black, width: 2.0)),
     );
   }
 }
