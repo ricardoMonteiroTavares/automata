61583422c4985156ebec1aeb2428e310c9d3a7a5
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-05-20 12:13:27 -0300

    Criação das funções que detectam se o cursor clicou em um estado ou não e caso tenha clicado, mudasse a cor do objeto

--- lib/window/IDEWindow/ideWindowController.dart
+++ lib/window/IDEWindow/ideWindowController.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:automata/widgets/stateWidget.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mobx/mobx.dart';
 
@@ -10,21 +11,73 @@ class IDEWindowController = _IDEWindowController with _$IDEWindowController;
 
 abstract class _IDEWindowController with Store {
   @observable
-  ObservableList<LayoutId> states = ObservableList<LayoutId>();
+  ObservableList<StateWidget> _states = ObservableList<StateWidget>();
+
+  @computed
+  List<LayoutId> get states =>
+      _states.map((e) => LayoutId(id: e.id, child: e)).toList();
 
   @observable
   ObservableMap<int, Offset> positions = ObservableMap<int, Offset>();
 
+  @observable
+  StateWidgetState? _selectedState;
+
   @action
-  void addState(TapDownDetails details) {
+  void add(TapDownDetails details) {
+    int id = _selectState(details);
+
+    if (id == -1) {
+      if (_selectedState != null) {
+        _selectedState!.updateColor();
+        _selectedState = null;
+      } else {
+        _addState(details);
+      }
+    }
+  }
+
+  @action
+  void _addState(TapDownDetails details) {
     int id;
-    if (states.isEmpty) {
+    if (_states.isEmpty) {
       id = 0;
     } else {
-      var obj = states[states.length - 1].id;
+      var obj = _states[_states.length - 1].id;
       id = int.parse(obj.toString()) + 1;
     }
     positions.addAll({id: details.localPosition});
-    states.add(LayoutId(id: states.length, child: StateWidget(name: "q$id")));
+    _states.add(StateWidget(
+      id: id,
+      name: "q$id",
+      f: _selected,
+    ));
+  }
+
+  @action
+  int _selectState(TapDownDetails details) {
+    Offset click = details.localPosition;
+
+    int id = -1;
+    for (int key in positions.keys) {
+      Offset statePosition = positions[key]!;
+      double distance = (statePosition - click).distance.abs();
+
+      if (distance <= 30) {
+        id = key;
+        break;
+      }
+    }
+
+    return id;
+  }
+
+  @action
+  void _selected(StateWidgetState selected) {
+    selected.updateColor();
+    if (_selectedState != null) {
+      _selectedState!.updateColor();
+    }
+    _selectedState = selected;
   }
 }
--- lib/window/IDEWindow/ideWindowController.g.dart
+++ lib/window/IDEWindow/ideWindowController.g.dart
@@ -9,18 +9,26 @@ part of 'ideWindowController.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$IDEWindowController on _IDEWindowController, Store {
-  final _$statesAtom = Atom(name: '_IDEWindowController.states');
+  Computed<List<LayoutId>>? _$statesComputed;
 
   @override
-  ObservableList<LayoutId> get states {
-    _$statesAtom.reportRead();
-    return super.states;
+  List<LayoutId> get states =>
+      (_$statesComputed ??= Computed<List<LayoutId>>(() => super.states,
+              name: '_IDEWindowController.states'))
+          .value;
+
+  final _$_statesAtom = Atom(name: '_IDEWindowController._states');
+
+  @override
+  ObservableList<StateWidget> get _states {
+    _$_statesAtom.reportRead();
+    return super._states;
   }
 
   @override
-  set states(ObservableList<LayoutId> value) {
-    _$statesAtom.reportWrite(value, super.states, () {
-      super.states = value;
+  set _states(ObservableList<StateWidget> value) {
+    _$_statesAtom.reportWrite(value, super._states, () {
+      super._states = value;
     });
   }
 
@@ -39,15 +47,64 @@ mixin _$IDEWindowController on _IDEWindowController, Store {
     });
   }
 
+  final _$_selectedStateAtom =
+      Atom(name: '_IDEWindowController._selectedState');
+
+  @override
+  StateWidgetState? get _selectedState {
+    _$_selectedStateAtom.reportRead();
+    return super._selectedState;
+  }
+
+  @override
+  set _selectedState(StateWidgetState? value) {
+    _$_selectedStateAtom.reportWrite(value, super._selectedState, () {
+      super._selectedState = value;
+    });
+  }
+
   final _$_IDEWindowControllerActionController =
       ActionController(name: '_IDEWindowController');
 
   @override
-  void addState(TapDownDetails details) {
+  void add(TapDownDetails details) {
     final _$actionInfo = _$_IDEWindowControllerActionController.startAction(
-        name: '_IDEWindowController.addState');
+        name: '_IDEWindowController.add');
     try {
-      return super.addState(details);
+      return super.add(details);
+    } finally {
+      _$_IDEWindowControllerActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void _addState(TapDownDetails details) {
+    final _$actionInfo = _$_IDEWindowControllerActionController.startAction(
+        name: '_IDEWindowController._addState');
+    try {
+      return super._addState(details);
+    } finally {
+      _$_IDEWindowControllerActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  int _selectState(TapDownDetails details) {
+    final _$actionInfo = _$_IDEWindowControllerActionController.startAction(
+        name: '_IDEWindowController._selectState');
+    try {
+      return super._selectState(details);
+    } finally {
+      _$_IDEWindowControllerActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void _selected(StateWidgetState selected) {
+    final _$actionInfo = _$_IDEWindowControllerActionController.startAction(
+        name: '_IDEWindowController._selected');
+    try {
+      return super._selected(selected);
     } finally {
       _$_IDEWindowControllerActionController.endAction(_$actionInfo);
     }
@@ -56,8 +113,8 @@ mixin _$IDEWindowController on _IDEWindowController, Store {
   @override
   String toString() {
     return '''
-states: ${states},
-positions: ${positions}
+positions: ${positions},
+states: ${states}
     ''';
   }
 }
