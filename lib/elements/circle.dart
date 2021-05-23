40ac28a4c2625010eaaa6cce1589d7026d1ac9c6
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-05-23 12:50:23 -0300

    Criação do elemento de círculo

---
+++ lib/elements/circle.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Circle extends StatelessWidget {
+  late final Widget _child;
+  late final double _size;
+  late final Color _color;
+
+  Circle({required double size, required Widget child, required Color color}) {
+    _child = child;
+    _size = size;
+    _color = color;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _size,
+      width: _size,
+      child: Center(
+        child: _child,
+      ),
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: _color, width: 2.0)),
+    );
+  }
+}
