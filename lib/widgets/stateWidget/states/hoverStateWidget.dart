f129067e89917239eb3033887be4c6a69aadc07c
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-06-13 15:59:53 -0300

    Substituição de abordagem para a detecção de um click no Pin

--- lib/widgets/stateWidget/states/hoverStateWidget.dart
+++ lib/widgets/stateWidget/states/hoverStateWidget.dart
@@ -1,15 +1,21 @@
 import 'package:automata/elements/pin.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 
 /// Objeto que só aparece quando o mouse está por cima do objeto
 class HoverStateWidget extends StatelessWidget {
   late final bool _hover;
-  late final Function(Offset) _slot;
-  HoverStateWidget(
-      {required bool hover, required Function(Offset) onPanStart}) {
+
+  HoverStateWidget({required bool hover}) {
     _hover = hover;
-    _slot = onPanStart;
   }
+
+  final Map<String, Offset> _pinPositions = {
+    "c1": Offset(25, -4),
+    "c2": Offset(25, 54),
+    "c3": Offset(54, 25),
+    "c4": Offset(-4, 25)
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -17,28 +23,41 @@ class HoverStateWidget extends StatelessWidget {
       visible: _hover,
       child: Stack(
         children: [
-          GestureDetector(
-            onPanStart: _onStart,
-            child: Pin(transform: Matrix4.translationValues(25, -4, 0)),
+          Transform.translate(
+            offset: _pinPositions["c1"]!,
+            child: Pin(),
           ),
-          GestureDetector(
-            onPanStart: _onStart,
-            child: Pin(transform: Matrix4.translationValues(25, 54, 0)),
+          Transform.translate(
+            offset: _pinPositions["c2"]!,
+            child: Pin(),
           ),
-          GestureDetector(
-            onPanStart: _onStart,
-            child: Pin(transform: Matrix4.translationValues(54, 25, 0)),
+          Transform.translate(
+            offset: _pinPositions["c3"]!,
+            child: Pin(),
           ),
-          GestureDetector(
-            onPanStart: _onStart,
-            child: Pin(transform: Matrix4.translationValues(-4, 25, 0)),
+          Transform.translate(
+            offset: _pinPositions["c4"]!,
+            child: Pin(),
           ),
         ],
       ),
     );
   }
 
-  void _onStart(DragStartDetails details) {
-    _slot(details.localPosition);
+  /// Função que verifica se o click foi realizado em cima de algum dos pins
+  Either<bool, Offset> clickInPin(Offset centerNode, Offset clickPosition) {
+    Offset topLeftNode = centerNode - Offset(30, 30);
+    for (String key in _pinPositions.keys) {
+      Offset topLeft = topLeftNode + _pinPositions[key]!;
+      Offset bottomRight = topLeftNode + Offset(10, 10);
+      if (topLeft.dx >= clickPosition.dx &&
+          clickPosition.dx <= bottomRight.dx &&
+          topLeft.dy >= clickPosition.dy &&
+          clickPosition.dy <= bottomRight.dy) {
+        return Right(topLeft + Offset(5, 5));
+      }
+    }
+
+    return Left(false);
   }
 }
