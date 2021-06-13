b768333c5b0293d3d65e0f0e26b55725355248a4
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-06-13 15:16:39 -0300

    Remoção do parâmetro no objeto Pin

--- lib/elements/pin.dart
+++ lib/elements/pin.dart
@@ -3,17 +3,11 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 /// Objeto que representa um "Pin"
 class Pin extends StatelessWidget {
-  late final Matrix4 _transform;
-  Pin({required Matrix4 transform}) {
-    _transform = transform;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 10,
       height: 10,
-      transform: _transform,
       child: SvgPicture.asset(
         "assets/pin.svg",
         width: 10,
