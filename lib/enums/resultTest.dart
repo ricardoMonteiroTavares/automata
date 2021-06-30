b4588557d19341005ddb73bcdb104b5ad8c32278
Ricardo Monteiro <ricardotavares51@hotmail.com>
2021-06-30 10:03:15 -0300

    Adição de documentação

--- lib/enums/resultTest.dart
+++ lib/enums/resultTest.dart
@@ -1,1 +1,2 @@
+/// Enumerador que retorna o resultado do teste
 enum ResultTest { failure, passed, error }
--- lib/enums/stateType.dart
+++ lib/enums/stateType.dart
@@ -1,1 +1,2 @@
+/// Enumerador que representa o tipo de estado no widget de estado
 enum StateType { end, start, normal, error }
